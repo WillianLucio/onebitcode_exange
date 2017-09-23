f297bc58a20b40442776d92ee58043482a41688e
Willian Lucio <willian.l.lucio@gmail.com>
2017-09-23 14:59:11 -0300

    bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
-//= require tether
 //= require popper
+//= require tether
 //= require bootstrap
+//= require turbolinks
 //= require_tree .
